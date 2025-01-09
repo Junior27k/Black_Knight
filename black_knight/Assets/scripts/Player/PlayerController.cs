78698e45122128862ee7c3b644078760b74f39d8
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-01-09 09:46:34 -0100

    Update PlayerController.cs
    Video 1
    Player COntroller script feito

--- black_knight/Assets/scripts/Player/PlayerController.cs
+++ black_knight/Assets/scripts/Player/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float moveX;
+
+    public float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        moveX = Input.GetAxisRaw("Horizontal");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void FixedUpdate()
+    {
+        move();
+    }
+
+    void move()
+    {
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
     }
 }
