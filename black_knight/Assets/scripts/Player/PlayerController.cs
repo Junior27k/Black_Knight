cbe195c8e5b6cf80f3e8c0a472be957667a12f73
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-01-11 15:20:58 -0100

    Update PlayerController.cs
    meu player está se movendo! assim.

--- black_knight/Assets/scripts/Player/PlayerController.cs
+++ black_knight/Assets/scripts/Player/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // moveX = Input.GetAxis("Horizontal");
+        moveX = Input.GetAxis("Horizontal");
 
     }
 
