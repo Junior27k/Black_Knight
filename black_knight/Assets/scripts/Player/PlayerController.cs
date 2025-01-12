683ec552811672fd9ff1df6db3dcd9e23c662509
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-01-12 17:45:43 -0100

    Update PlayerController.cs
    jump

--- black_knight/Assets/scripts/Player/PlayerController.cs
+++ black_knight/Assets/scripts/Player/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private float moveX;
 
     public float speed;
+    public int addJumps;
+    public bool isGrounded;
+    public float JumpForce = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +23,52 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveX = Input.GetAxisRaw("Horizontal");
+
     }
 
     void FixedUpdate()
     {
         move();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                addJumps = 2;
+            }
+            Jump();
+        }
+
     }
+
 
     void move()
     {
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (addJumps > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            addJumps--;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+         if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
