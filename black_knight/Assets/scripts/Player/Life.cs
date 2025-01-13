37be09dd5c500984ffd8f6887f89eaa5c63ecf9c
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-01-13 16:47:13 -0100

    Life Script and Prefab

--- black_knight/Assets/scripts/Player/Life.cs
+++ black_knight/Assets/scripts/Player/Life.cs
@@ -15,4 +15,14 @@ public class Life : MonoBehaviour
     {
         
     }
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            player.life++;
+            Destroy(this.gameObject);
+        }
+    }
 }
