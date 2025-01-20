431f83567eb92b0d9b538c69b2604df801cdecde
Junior27k <136364434+Junior27k@users.noreply.github.com>
2025-01-20 14:03:05 -0100

    minhas mudanças
    fudeu tudo

---
+++ black_knight/Assets/scripts/Globals/GameOver.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public string level;
+     public void Restart() 
+    {
+        SceneManager.LoadScene(level);
+    }
+    
+
+    public void BackaToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ black_knight/Assets/scripts/Menu/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+    }
+
+}
