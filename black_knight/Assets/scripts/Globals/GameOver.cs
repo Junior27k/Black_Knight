69ad1f5808c56dc6826509c7bba9821cc97f5677
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-01-20 15:58:27 -0100

    Reapply "Merge branch 'fabio'"
    This reverts commit ad7a2d30aaf3c578f2e38a46409cde1fc017bf62.

--- black_knight/Assets/scripts/Globals/GameOver.cs
+++ black_knight/Assets/scripts/Globals/GameOver.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string level;
+     public void Restart() 
     {
-        
+        SceneManager.LoadScene(level);
+    }
+    
+
+    public void BackaToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
