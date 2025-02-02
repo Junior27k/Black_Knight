d2d7500641300242bb58eb7816b7f3ed2f1d3d75
Eril TS Carvalho <erilandocarvalho@gmail.com>
2025-02-02 18:41:00 -0100

    Gizmo script fix

--- black_knight/Assets/scripts/Enemies/Gizmo/GizmoKitten.cs
+++ black_knight/Assets/scripts/Enemies/Gizmo/GizmoKitten.cs
@@ -1,18 +1,32 @@
+@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GizmoKitten : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform gizmo;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
+        if(collision.CompareTag("Player"))
+        {
+            gizmo.GetComponent<GizmoController>().enabled= true;
+            gizmo.GetComponent<Animator>().SetBool("IsRun", true);
+        }
     }
 
     // Update is called once per frame
     void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
         
+        if(collision.CompareTag("Player"))
+        {
+            gizmo.GetComponent<GizmoController>().enabled= false;
+            gizmo.GetComponent<Animator>().SetBool("IsRun", false);
+        }
     }
 }
